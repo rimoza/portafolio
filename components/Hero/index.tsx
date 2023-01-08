f5d85e9c0b1e462b2d50d1cb19b1351cd2cb7e25
rimoza <ridwan.maxamed1@gmail.com>
2023-01-08 02:38:23 -0800

    fix: hero section style fix

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -4,9 +4,9 @@ import Container from "../Container/Container";
 
 const Hero = () => {
   return (
-    <div className="bg-gray-100">
+    <div className="mx-20">
         <Container>
-        <div className="flex flex-col md:flex-row justify-center items-center py-16 rounded-md">
+        <div className="flex flex-col md:flex-row justify-between items-center py-10">
         <div>
           <div>
             <h2 className="text-2xl">
@@ -14,7 +14,7 @@ const Hero = () => {
             </h2>
             <h1 className="text-6xl font-bold my-5">Web developer</h1>
           </div>
-          <div className="leading-7 max-w-4xl">
+          <div className="leading-7 max-w-2xl">
             <p>
               Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus,
               tempora magnam possimus ullam veniam harum quam sint temporibus
@@ -26,12 +26,13 @@ const Hero = () => {
             <button>Connect with me</button>
           </div>
         </div>
-        <div className="mt-10 md:mt-0">
+        <div className="flex flex-wrap justify-center md:ml-20">
           <Image
             src="https://images.unsplash.com/photo-1618477388954-7852f32655ec?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80"
             width={500}
             height={500}
             alt="hero"
+            className="h-96 max-w-md transition-all duration-300 rounded-lg cursor-pointer filter grayscale hover:grayscale-0"
           />
         </div>
         </div>
