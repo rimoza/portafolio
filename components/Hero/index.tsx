02f71d0693f2f4309496edc5509df5d11e5a9e13
rimoza <ridwan.maxamed1@gmail.com>
2023-01-11 01:13:15 -0800

    update hero

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -3,16 +3,14 @@ import Image from "next/image";
 
 const Hero = () => {
   return (
-    <div className="hero mt-5">
+    <div className="hero my-24 h-1/2">
       <div className="hero-content text-center">
-        <div className="max-w-md">
-          <h1 className="text-5xl font-bold">Hello there</h1>
+        <div className="max-w-2xl">
+          <h1 className="text-5xl font-bold">HELLO THERE I&apos;M RIDWAN MOHAMED</h1>
           <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
+          A Frontend focused Web Developer building the Frontend of Websites and Web Applications that leads to the success of the overall product
           </p>
-          <button className="btn btn-primary">Get Started</button>
+          <button className="btn btn-primary">PROJECTS</button>
         </div>
       </div>
     </div>
