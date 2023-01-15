238696cb80abc9fb95d1a9365aa8e3c669396776
rimoza <ridwan.maxamed1@gmail.com>
2023-01-14 23:25:16 -0800

    fix: Style fix

--- components/Skills/index.tsx
+++ components/Skills/index.tsx
@@ -3,11 +3,11 @@ import React from "react";
 const SkillList = ({ skills }: any) => {
   return (
     <>
-      <ul className="grid grid-cols-2 md:grid-cols-3 leading-7 text-md text-gray-500 font-semibold">
+      <ul className="grid grid-cols-2 leading-7 text-md text-gray-500 font-semibold">
         {skills.map((skill: any) => (
           <div key={skill.name}>
             <li className='flex flex-row'>
-              <span className="bg-gray-300 py-2 px-5 m-2 rounded-md drop-shadow-sm">
+              <span className="bg-gray-300 w-full max-w-md py-2 px-5 m-2 rounded-md drop-shadow-sm">
               {skill.name}
               </span>
             </li>
