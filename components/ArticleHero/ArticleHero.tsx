afb8ada5ae7e0f484284d9caf33a645f1a134a5b
rimoza <ridwan.maxamed1@gmail.com>
2023-02-12 14:20:01 -0800

    feat: add article hero component

---
+++ components/ArticleHero/ArticleHero.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const ArticleHero = ({ title, imageUrl }: { title: any; imageUrl: any }) => {
+  return (
+    <div
+      className="bg-cover bg-center h-64 my-20"
+      style={{ backgroundImage: `url(${imageUrl})` }}
+    >
+      <div className="flex items-center justify-center h-full px-6 py-4 bg-bg opacity-75">
+        <h1 className="text-3xl font-bold text-[#fff]">{title}</h1>
+      </div>
+    </div>
+  );
+};
+
+export default ArticleHero;
