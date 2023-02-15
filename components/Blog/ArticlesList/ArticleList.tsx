e1aa21383fee49d86165f77b7cac9fd32f92dfc7
Ridwan Mohamed <ridwan.maxamed1@gmail.com>
2023-02-15 00:13:07 -0800

    Update ArticleList.tsx

--- components/Blog/ArticlesList/ArticleList.tsx
+++ components/Blog/ArticlesList/ArticleList.tsx
@@ -12,7 +12,7 @@ const ArticleList = ({ blogs }: { blogs: IBlogPostData[] }) => {
     <Container>
         <ArticleHero title='All Articles' imageUrl="/images/box3.png" />
       <div className="my-10">
-        <Heading>Articles</Heading>
+        <Heading>Under construction</Heading>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 mx-5 md:mx-0">
         {blogs.map((article: IBlogPostData) => (
