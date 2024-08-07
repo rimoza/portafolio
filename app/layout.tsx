43916a09b6eb6ef363baba22fa13b87d03578b6e
Ridwan Mohamed <ridwan.maxamed1@gmail.com>
2024-08-07 17:57:14 +0300

    feat: add vercel speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import { Alegreya } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import Layout from "../components/Layout/Layout";
 import "./styles/globals.css";
 
@@ -16,6 +17,7 @@ export default function RootLayout({
         <Layout className={alegreya.className}>
           {children}
           <Analytics />
+          <SpeedInsights />
         </Layout>
       </body>
     </html>
